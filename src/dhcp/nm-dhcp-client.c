b431cd3dbb6fff89d53699d2472ce6132393b34a
Beniamino Galvani <bgalvani@redhat.com>
2017-04-26 11:49:30 +0200

    dhcp: simplify how hostname and FQDN are passed down to backends
    Since they are mutually exclusive, pass a string and a boolean to
    indicate whether we want to use the hostname or the FQDN option.

--- src/dhcp/nm-dhcp-client.c
+++ src/dhcp/nm-dhcp-client.c
@@ -68,7 +68,7 @@ typedef struct _NMDhcpClientPrivate {
 	GByteArray * duid;
 	GBytes *     client_id;
 	char *       hostname;
-	char *       fqdn;
+	gboolean     use_fqdn;
 
 	NMDhcpState  state;
 	pid_t        pid;
@@ -178,12 +178,12 @@ nm_dhcp_client_get_hostname (NMDhcpClient *self)
 	return NM_DHCP_CLIENT_GET_PRIVATE (self)->hostname;
 }
 
-const char *
-nm_dhcp_client_get_fqdn (NMDhcpClient *self)
+gboolean
+nm_dhcp_client_get_use_fqdn (NMDhcpClient *self)
 {
-	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), NULL);
+	g_return_val_if_fail (NM_IS_DHCP_CLIENT (self), FALSE);
 
-	return NM_DHCP_CLIENT_GET_PRIVATE (self)->fqdn;
+	return NM_DHCP_CLIENT_GET_PRIVATE (self)->use_fqdn;
 }
 
 /*****************************************************************************/
@@ -408,7 +408,7 @@ nm_dhcp_client_start_ip4 (NMDhcpClient *self,
                           const char *dhcp_client_id,
                           const char *dhcp_anycast_addr,
                           const char *hostname,
-                          const char *fqdn,
+                          gboolean use_fqdn,
                           const char *last_ip4_address)
 {
 	NMDhcpClientPrivate *priv;
@@ -429,8 +429,7 @@ nm_dhcp_client_start_ip4 (NMDhcpClient *self,
 
 	g_clear_pointer (&priv->hostname, g_free);
 	priv->hostname = g_strdup (hostname);
-	g_free (priv->fqdn);
-	priv->fqdn = g_strdup (fqdn);
+	priv->use_fqdn = use_fqdn;
 
 	return NM_DHCP_CLIENT_GET_CLASS (self)->ip4_start (self, dhcp_anycast_addr, last_ip4_address);
 }
@@ -903,7 +902,6 @@ dispose (GObject *object)
 
 	g_clear_pointer (&priv->iface, g_free);
 	g_clear_pointer (&priv->hostname, g_free);
-	g_clear_pointer (&priv->fqdn, g_free);
 	g_clear_pointer (&priv->uuid, g_free);
 	g_clear_pointer (&priv->client_id, g_bytes_unref);
 
